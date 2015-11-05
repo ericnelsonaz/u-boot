73f5ce6922679487b61f1eb89b01b7b0cc663ea0
Eric Nelson <eric@nelint.com>
2015-11-04 18:02:10 -0700

    ARM: imx: mx6memcal: consolidate drive strength and mode constants
    Signed-off-by: Eric Nelson <eric@nelint.com>

--- board/freescale/mx6memcal/mx6memcal.c
+++ board/freescale/mx6memcal/mx6memcal.c
@@ -50,6 +50,12 @@ static iomux_v3_cfg_t const uart_pads[] = {
 #define GRP_DDRTYPE	0x00080000
 #endif
 
+#define DDR_PKE		0
+#define DDR_MODE	0x00020000
+
+#define DRAM_DRIVE_STRENGTH \
+	(CONFIG_DRAM_DRIVE_STRENGTH << 3)
+
 #define mmdc0 ((struct mmdc_p_regs volatile *)MMDC_P0_BASE_ADDR)
 #define mmdc1 ((struct mmdc_p_regs volatile *)MMDC_P1_BASE_ADDR)
 #define mx6q_ddrmux ((struct mx6dq_iomux_ddr_regs volatile *)MX6DQ_IOM_DDR_BASE)
@@ -537,97 +543,97 @@ static int mmdc_do_dqs_calibration
 /* configure MX6Q/DUAL mmdc DDR io registers */
 static struct mx6dq_iomux_ddr_regs const mx6dq_ddr_ioregs = {
 	/* SDCLK[0:1], CAS, RAS, Reset: Differential input, 40ohm */
-	.dram_sdclk_0 = 0x00020030,
-	.dram_sdclk_1 = 0x00020030,
-	.dram_cas = 0x00020030,
-	.dram_ras = 0x00020030,
-	.dram_reset = 0x00020030,
+	.dram_sdclk_0 = DDR_MODE+DRAM_DRIVE_STRENGTH,
+	.dram_sdclk_1 = DDR_MODE+DRAM_DRIVE_STRENGTH,
+	.dram_cas = DDR_MODE+DRAM_DRIVE_STRENGTH,
+	.dram_ras = DDR_MODE+DRAM_DRIVE_STRENGTH,
+	.dram_reset = DDR_MODE+DRAM_DRIVE_STRENGTH,
 	/* SDCKE[0:1]: 100k pull-up */
 	.dram_sdcke0 = 0x00003000,
 	.dram_sdcke1 = 0x00003000,
 	/* SDBA2: pull-up disabled */
 	.dram_sdba2 = 0x00000000,
 	/* SDODT[0:1]: 100k pull-up, 40 ohm */
-	.dram_sdodt0 = 0x00003030,
-	.dram_sdodt1 = 0x00003030,
+	.dram_sdodt0 = 0x00003000+DRAM_DRIVE_STRENGTH,
+	.dram_sdodt1 = 0x00003000+DRAM_DRIVE_STRENGTH,
 	/* SDQS[0:7]: Differential input, 40 ohm */
-	.dram_sdqs0 = 0x00000030,
-	.dram_sdqs1 = 0x00000030,
-	.dram_sdqs2 = 0x00000030,
-	.dram_sdqs3 = 0x00000030,
-	.dram_sdqs4 = 0x00000030,
-	.dram_sdqs5 = 0x00000030,
-	.dram_sdqs6 = 0x00000030,
-	.dram_sdqs7 = 0x00000030,
+	.dram_sdqs0 = DRAM_DRIVE_STRENGTH,
+	.dram_sdqs1 = DRAM_DRIVE_STRENGTH,
+	.dram_sdqs2 = DRAM_DRIVE_STRENGTH,
+	.dram_sdqs3 = DRAM_DRIVE_STRENGTH,
+	.dram_sdqs4 = DRAM_DRIVE_STRENGTH,
+	.dram_sdqs5 = DRAM_DRIVE_STRENGTH,
+	.dram_sdqs6 = DRAM_DRIVE_STRENGTH,
+	.dram_sdqs7 = DRAM_DRIVE_STRENGTH,
 
 	/* DQM[0:7]: Differential input, 40 ohm */
-	.dram_dqm0 = 0x00020030,
-	.dram_dqm1 = 0x00020030,
-	.dram_dqm2 = 0x00020030,
-	.dram_dqm3 = 0x00020030,
-	.dram_dqm4 = 0x00020030,
-	.dram_dqm5 = 0x00020030,
-	.dram_dqm6 = 0x00020030,
-	.dram_dqm7 = 0x00020030,
+	.dram_dqm0 = DDR_MODE+DRAM_DRIVE_STRENGTH,
+	.dram_dqm1 = DDR_MODE+DRAM_DRIVE_STRENGTH,
+	.dram_dqm2 = DDR_MODE+DRAM_DRIVE_STRENGTH,
+	.dram_dqm3 = DDR_MODE+DRAM_DRIVE_STRENGTH,
+	.dram_dqm4 = DDR_MODE+DRAM_DRIVE_STRENGTH,
+	.dram_dqm5 = DDR_MODE+DRAM_DRIVE_STRENGTH,
+	.dram_dqm6 = DDR_MODE+DRAM_DRIVE_STRENGTH,
+	.dram_dqm7 = DDR_MODE+DRAM_DRIVE_STRENGTH,
 };
 
 /* configure MX6Q/DUAL mmdc GRP io registers */
 static struct mx6dq_iomux_grp_regs const mx6dq_grp_ioregs = {
 	/* DDR3 */
 	.grp_ddr_type = GRP_DDRTYPE,
-	.grp_ddrmode_ctl = 0x00020000,
+	.grp_ddrmode_ctl = DDR_MODE,
 	/* disable DDR pullups */
-	.grp_ddrpke = 0x00000000,
+	.grp_ddrpke = DDR_PKE,
 	/* ADDR[00:16], SDBA[0:1]: 40 ohm */
-	.grp_addds = 0x00000030,
+	.grp_addds = DRAM_DRIVE_STRENGTH,
 	/* CS0/CS1/SDBA2/CKE0/CKE1/SDWE: 40 ohm */
-	.grp_ctlds = 0x00000030,
+	.grp_ctlds = DRAM_DRIVE_STRENGTH,
 	/* DATA[00:63]: Differential input, 40 ohm */
-	.grp_ddrmode = 0x00020000,
-	.grp_b0ds = 0x00000030,
-	.grp_b1ds = 0x00000030,
-	.grp_b2ds = 0x00000030,
-	.grp_b3ds = 0x00000030,
-	.grp_b4ds = 0x00000030,
-	.grp_b5ds = 0x00000030,
-	.grp_b6ds = 0x00000030,
-	.grp_b7ds = 0x00000030,
+	.grp_ddrmode = DDR_MODE,
+	.grp_b0ds = DRAM_DRIVE_STRENGTH,
+	.grp_b1ds = DRAM_DRIVE_STRENGTH,
+	.grp_b2ds = DRAM_DRIVE_STRENGTH,
+	.grp_b3ds = DRAM_DRIVE_STRENGTH,
+	.grp_b4ds = DRAM_DRIVE_STRENGTH,
+	.grp_b5ds = DRAM_DRIVE_STRENGTH,
+	.grp_b6ds = DRAM_DRIVE_STRENGTH,
+	.grp_b7ds = DRAM_DRIVE_STRENGTH,
 };
 
 static struct mx6sdl_iomux_ddr_regs const mx6sdl_ddr_ioregs = {
 	/* SDCLK[0:1], CAS, RAS, Reset: Differential input, 40ohm */
-	.dram_sdclk_0 = 0x00020030,
-	.dram_sdclk_1 = 0x00020030,
-	.dram_cas = 0x00020030,
-	.dram_ras = 0x00020030,
-	.dram_reset = 0x00020030,
+	.dram_sdclk_0 = DDR_MODE+DRAM_DRIVE_STRENGTH,
+	.dram_sdclk_1 = DDR_MODE+DRAM_DRIVE_STRENGTH,
+	.dram_cas = DDR_MODE+DRAM_DRIVE_STRENGTH,
+	.dram_ras = DDR_MODE+DRAM_DRIVE_STRENGTH,
+	.dram_reset = DDR_MODE+DRAM_DRIVE_STRENGTH,
 	/* SDCKE[0:1]: 100k pull-up */
 	.dram_sdcke0 = 0x00003000,
 	.dram_sdcke1 = 0x00003000,
 	/* SDBA2: pull-up disabled */
 	.dram_sdba2 = 0x00000000,
 	/* SDODT[0:1]: 100k pull-up, 40 ohm */
-	.dram_sdodt0 = 0x00003030,
-	.dram_sdodt1 = 0x00003030,
+	.dram_sdodt0 = 0x00003000+DRAM_DRIVE_STRENGTH,
+	.dram_sdodt1 = 0x00003000+DRAM_DRIVE_STRENGTH,
 	/* SDQS[0:7]: Differential input, 40 ohm */
-	.dram_sdqs0 = 0x00000030,
-	.dram_sdqs1 = 0x00000030,
-	.dram_sdqs2 = 0x00000030,
-	.dram_sdqs3 = 0x00000030,
-	.dram_sdqs4 = 0x00000030,
-	.dram_sdqs5 = 0x00000030,
-	.dram_sdqs6 = 0x00000030,
-	.dram_sdqs7 = 0x00000030,
+	.dram_sdqs0 = DRAM_DRIVE_STRENGTH,
+	.dram_sdqs1 = DRAM_DRIVE_STRENGTH,
+	.dram_sdqs2 = DRAM_DRIVE_STRENGTH,
+	.dram_sdqs3 = DRAM_DRIVE_STRENGTH,
+	.dram_sdqs4 = DRAM_DRIVE_STRENGTH,
+	.dram_sdqs5 = DRAM_DRIVE_STRENGTH,
+	.dram_sdqs6 = DRAM_DRIVE_STRENGTH,
+	.dram_sdqs7 = DRAM_DRIVE_STRENGTH,
 
 	/* DQM[0:7]: Differential input, 40 ohm */
-	.dram_dqm0 = 0x00020030,
-	.dram_dqm1 = 0x00020030,
-	.dram_dqm2 = 0x00020030,
-	.dram_dqm3 = 0x00020030,
-	.dram_dqm4 = 0x00020030,
-	.dram_dqm5 = 0x00020030,
-	.dram_dqm6 = 0x00020030,
-	.dram_dqm7 = 0x00020030,
+	.dram_dqm0 = DDR_MODE+DRAM_DRIVE_STRENGTH,
+	.dram_dqm1 = DDR_MODE+DRAM_DRIVE_STRENGTH,
+	.dram_dqm2 = DDR_MODE+DRAM_DRIVE_STRENGTH,
+	.dram_dqm3 = DDR_MODE+DRAM_DRIVE_STRENGTH,
+	.dram_dqm4 = DDR_MODE+DRAM_DRIVE_STRENGTH,
+	.dram_dqm5 = DDR_MODE+DRAM_DRIVE_STRENGTH,
+	.dram_dqm6 = DDR_MODE+DRAM_DRIVE_STRENGTH,
+	.dram_dqm7 = DDR_MODE+DRAM_DRIVE_STRENGTH,
 };
 
 /* configure MX6SOLO/DUALLITE mmdc GRP io registers */
@@ -635,53 +641,53 @@ static struct mx6sdl_iomux_grp_regs const mx6sdl_grp_ioregs = {
 	/* DDR3 */
 	.grp_ddr_type = GRP_DDRTYPE,
 	/* SDQS[0:7]: Differential input, 40 ohm */
-	.grp_ddrmode_ctl = 0x00020000,
+	.grp_ddrmode_ctl = DDR_MODE,
 	/* disable DDR pullups */
-	.grp_ddrpke = 0x00000000,
+	.grp_ddrpke = DDR_PKE,
 	/* ADDR[00:16], SDBA[0:1]: 40 ohm */
-	.grp_addds = 0x00000030,
+	.grp_addds = DRAM_DRIVE_STRENGTH,
 	/* CS0/CS1/SDBA2/CKE0/CKE1/SDWE: 40 ohm */
-	.grp_ctlds = 0x00000030,
+	.grp_ctlds = DRAM_DRIVE_STRENGTH,
 	/* DATA[00:63]: Differential input, 40 ohm */
-	.grp_ddrmode = 0x00020000,
-	.grp_b0ds = 0x00000030,
-	.grp_b1ds = 0x00000030,
-	.grp_b2ds = 0x00000030,
-	.grp_b3ds = 0x00000030,
-	.grp_b4ds = 0x00000030,
-	.grp_b5ds = 0x00000030,
-	.grp_b6ds = 0x00000030,
-	.grp_b7ds = 0x00000030,
+	.grp_ddrmode = DDR_MODE,
+	.grp_b0ds = DRAM_DRIVE_STRENGTH,
+	.grp_b1ds = DRAM_DRIVE_STRENGTH,
+	.grp_b2ds = DRAM_DRIVE_STRENGTH,
+	.grp_b3ds = DRAM_DRIVE_STRENGTH,
+	.grp_b4ds = DRAM_DRIVE_STRENGTH,
+	.grp_b5ds = DRAM_DRIVE_STRENGTH,
+	.grp_b6ds = DRAM_DRIVE_STRENGTH,
+	.grp_b7ds = DRAM_DRIVE_STRENGTH,
 };
 
 const struct mx6sl_iomux_ddr_regs mx6sl_ddr_ioregs = {
-	.dram_sdqs0 = 0x00003030,
-	.dram_sdqs1 = 0x00003030,
-	.dram_sdqs2 = 0x00003030,
-	.dram_sdqs3 = 0x00003030,
-	.dram_dqm0 = 0x00000030,
-	.dram_dqm1 = 0x00000030,
-	.dram_dqm2 = 0x00000030,
-	.dram_dqm3 = 0x00000030,
-	.dram_cas  = 0x00000030,
-	.dram_ras  = 0x00000030,
-	.dram_sdclk_0 = 0x00000028,
-	.dram_reset = 0x00000030,
-	.dram_sdba2 = 0x00000000,
-	.dram_odt0 = 0x00000008,
-	.dram_odt1 = 0x00000008,
+	.dram_sdqs0 = DRAM_DRIVE_STRENGTH,
+	.dram_sdqs1 = DRAM_DRIVE_STRENGTH,
+	.dram_sdqs2 = DRAM_DRIVE_STRENGTH,
+	.dram_sdqs3 = DRAM_DRIVE_STRENGTH,
+	.dram_dqm0 = DRAM_DRIVE_STRENGTH,
+	.dram_dqm1 = DRAM_DRIVE_STRENGTH,
+	.dram_dqm2 = DRAM_DRIVE_STRENGTH,
+	.dram_dqm3 = DRAM_DRIVE_STRENGTH,
+	.dram_cas  = DRAM_DRIVE_STRENGTH,
+	.dram_ras  = DRAM_DRIVE_STRENGTH,
+	.dram_sdclk_0 = DRAM_DRIVE_STRENGTH,
+	.dram_reset = DRAM_DRIVE_STRENGTH,
+	.dram_sdba2 = 0x00020000,
+	.dram_odt0 = 0x00030000+DRAM_DRIVE_STRENGTH,
+	.dram_odt1 = 0x00030000+DRAM_DRIVE_STRENGTH,
 };
 
 const struct mx6sl_iomux_grp_regs mx6sl_grp_ioregs = {
-	.grp_b0ds = 0x00000030,
-	.grp_b1ds = 0x00000030,
-	.grp_b2ds = 0x00000030,
-	.grp_b3ds = 0x00000030,
-	.grp_addds = 0x00000030,
-	.grp_ctlds = 0x00000030,
-	.grp_ddrmode_ctl = 0x00020000,
-	.grp_ddrpke = 0x00000000,
-	.grp_ddrmode = 0x00020000,
+	.grp_b0ds = DRAM_DRIVE_STRENGTH,
+	.grp_b1ds = DRAM_DRIVE_STRENGTH,
+	.grp_b2ds = DRAM_DRIVE_STRENGTH,
+	.grp_b3ds = DRAM_DRIVE_STRENGTH,
+	.grp_addds = DRAM_DRIVE_STRENGTH,
+	.grp_ctlds = DRAM_DRIVE_STRENGTH,
+	.grp_ddrmode_ctl = DDR_MODE,
+	.grp_ddrpke = DDR_PKE,
+	.grp_ddrmode = DDR_MODE,
 	.grp_ddr_type = GRP_DDRTYPE,
 };
 
