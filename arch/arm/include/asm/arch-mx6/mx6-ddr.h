9fee542e0ac6f8e14feabf12afff0414478dfee7
Eric Nelson <eric@nelint.com>
2016-10-27 07:44:09 -0700

    ARM: mx6: ddr: add plugin-utils placeholder
    Add entry points for saving the state of the machine at entry
    from the Boot ROM and for restoring the state before a return.
    
    Note that this needs some fixup before it's useful, so I'm
    forwarding it as an RFC to solicit advice.
    
    This placeholder is little more than a setjmp/longjmp that
    saves the SP, LR and registers 0-9. Disassembling the ROM
    for i.MX6DL and i.MX6SL shows that these are the only registers
    used by the ROM on those SOCs.
    
    Signed-off-by: Eric Nelson <eric@nelint.com>

--- arch/arm/include/asm/arch-mx6/mx6-ddr.h
+++ arch/arm/include/asm/arch-mx6/mx6-ddr.h
@@ -528,4 +528,23 @@ void mx6_dram_cfg(const struct mx6_ddr_sysinfo *,
 #define MX6_MMDC_P1_MPZQLP2CTL	0x021b485C
 #define MX6_MMDC_P1_MPMUR0	0x021b48b8
 
+#ifdef CONFIG_SPL_BUILD
+struct plugin_state {
+	uint32_t r0;
+	uint32_t r1;
+	uint32_t r2;
+	uint32_t r3;
+	uint32_t r4;
+	uint32_t r5;
+	uint32_t r6;
+	uint32_t r7;
+	uint32_t r8;
+	uint32_t r9;
+	uint32_t fp;
+	uint32_t sp;
+	uint32_t lr;
+};
+void return_to_rom(void);
+#endif
+
 #endif	/*__ASM_ARCH_MX6_DDR_H__ */
